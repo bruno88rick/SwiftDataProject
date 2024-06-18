b439cabd8103fbc7464efcfe219550874dc5c861
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-06-18 05:54:26 -0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Bruno Oliveira on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
